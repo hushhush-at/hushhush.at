82c69e6c13f7ae1e82925858ef4e64ce501c37c2
Marcello <marcello@roark.at>
2024-05-16 10:51:08 +0200

    add react and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), mdx()]
 });
